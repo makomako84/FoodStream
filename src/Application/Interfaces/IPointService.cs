b33a20b6755bba68371191c2ff2afeb84bec5cbe
hanzzbrr <artur.galimsky@gmail.com>
2022-11-02 13:05:14 +0500

    Application: response, dto, interface

---
+++ src/Application/Interfaces/IPointService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Foodstream.Application.DTO;
+
+namespace Foodstream.Application.Interfaces;
+
+public interface IPointService
+{
+    PointResponse Add(string address);
+    PointResponse Update(int id, string address);
+    List<PointResponse> List();
+}
---
+++ src/Application/Services/PointService.cs
@@ -0,0 +1,26 @@
+﻿using Foodstream.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodstream.Application.Services;
+
+internal class PointService : IPointService
+{
+    public void Add()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void List()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
