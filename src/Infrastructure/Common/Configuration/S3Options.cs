a765e02577573293d7554a82d204195f1535ac27
hanzzbrr <artur.galimsky@gmail.com>
2022-11-06 00:43:00 +0500

    remove s3 options

--- src/Infrastructure/Common/Configuration/S3Options.cs
+++
@@ -1,9 +0,0 @@
-namespace Foodstream.Infrastructure.Configuration
-{
-    public class S3Options
-    {
-        public const string Section = "S3Settings";
-        public string BucketName { get; set; }
-        public string PointPrefix { get; set; }
-    }
-}
